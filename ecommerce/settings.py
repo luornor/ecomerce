18ad8869e2091eb8a91a517833793be1c8d258c6
Tettey1 <tetteynathan89@gmail.com>
2024-06-11 21:12:23 +0000

    modified:   ecommerce/__pycache__/settings.cpython-312.pyc 	modified:   ecommerce/settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -101,7 +102,7 @@ DATABASES = {
     }
 }
 DATABASES={
-    'default':dj_database_url.parse("h281dP6uk7vmhlMr0g1u41zpYsSOLQxD@dpg-cpkaojkf7o1s73clumeg-a.oregon-postgres.render.com/jiji_ecomerce")
+    'default':dj_database_url.parse("postgres://jiji_ecomerce_user:h281dP6uk7vmhlMr0g1u41zpYsSOLQxD@dpg-cpkaojkf7o1s73clumeg-a.oregon-postgres.render.com/jiji_ecomerce")
 }
 
 # Password validation
