47927fb41bbb8b67005063bc31e834eb6e93e0c7
Tettey1 <tetteynathan89@gmail.com>
2024-06-18 13:30:54 +0000

    modified:   ecommerce/settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -100,9 +100,7 @@ DATABASES = {
 }
 
 # To use the DATABASE_URL
-DATABASES['default'] = dj_database_url.parse(
-    config('DATABASE_URL',default='postgres://myuser:/mydatabase')
-    )
+DATABASES['default'] = dj_database_url.parse('postgres://jiji_ecomerce_user:h281dP6uk7vmhlMr0g1u41zpYsSOLQxD@dpg-cpkaojkf7o1s73clumeg-a/jiji_ecomerce')
 
 
 # Password validation
